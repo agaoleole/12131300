b33e702bfee3eb56fea9dd4ef3545c94112b73a9
agaoleole <aga.rizky29@gmail.com>
2016-03-30 10:58:48 +0700

    Added files via upload

---
+++ Dinamis.php
@@ -0,0 +1,37 @@
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Untitled Document</title>
+</head>
+
+<body>
+<table width="538" border="1">
+  <tr>
+  	
+    <td width="38" bgcolor="#0000FF">No</td>
+    <td width="172" bgcolor="#0000FF">Nama</td>
+    <td width="306" bgcolor="#0000FF">Alamat</td>
+  </tr>
+  <tr>
+    <td bgcolor="#999999">1</td>
+    <td bgcolor="#999999">Bayu</td>
+    <td bgcolor="#999999">Bantul</td>
+  </tr>
+  <tr>
+    <td>2</td>
+    <td>Aga</td>
+    <td>Minomartani</td>
+  </tr>
+  <tr>
+    <td bgcolor="#999999">3</td>
+    <td bgcolor="#999999">Anis</td>
+    <td bgcolor="#999999">Kulonprogo</td>
+  </tr>
+  <tr>
+    <td>4</td>
+    <td>Eko</td>
+    <td>Bantul</td>
+  </tr>
+</table>
+</body>
+</html>
